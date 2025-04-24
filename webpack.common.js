40b0f64abf15d12cf5464e706f06800033684875
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-04-23 22:39:08 -0500

    Following the next lesson about webpack-merge
    Instead of having one webpack.config.js file that
    you have to manually change the mode in develoment
    or production, you can make 3 files, one
    webpack.common.js to import all of the plugins
    and modules as well as the entry point and
    output you need to set for both development and
    production modes, then one js file for development
    and one js file for production. Then using webpack
    merge, you can merge common with dev or production
    js files and switch between modes by setting a
    npm script inside package.json for npm. Need to
    redeploy project after committing and pushing.

--- webpack.common.js
+++ webpack.common.js
@@ -3,16 +3,11 @@ const path = require("path"); //Used to define an absolute path always to the "d
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development", //What mode we are in (production, development, etc.)
     entry: "./src/index.js", //"entry point" which is the highest on the dependency graph for imported files
     output: {
         filename: "index.js", //The output controls where the bundle's js is going and what the name of the bundled js is going to be.
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    },
-    devtool: "eval-source-map", //source map, used with webpack server to link errors to the right source.
-    devServer: {
-        watchFiles: ["./src/index.html"], //Since webpack server only tracks changes to js files, we have to tell it manually to watch html file as well.
     },
     plugins: [
         new HtmlWebpackPlugin({ //html webpack, to bundle our html file together
